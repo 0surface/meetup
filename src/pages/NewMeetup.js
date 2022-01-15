de755328707cfe2164983a6fb7503a079d2569a8
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2022-01-15 10:09:45 +0000

    sendin ga POST http request to Firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,13 +2,24 @@ import React from 'react'
 import NewMeetupform from '../components/meetups/New MeetupForm'
 
 const NewMeetupPage = () => {
-    const addMeetupHandler = (meetupData) =>{
-        
-    }
+  const { REACT_APP_FIREBASEAPIURL } = process.env
+
+  const addMeetupHandler = (meetupData) => {
+    let postUrl = REACT_APP_FIREBASEAPIURL + '/meetups.json'
+
+    fetch(postUrl, {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupform onAddMeetup={} />
+      <NewMeetupform onAddMeetup={addMeetupHandler} />
     </section>
   )
 }
